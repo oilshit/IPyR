5ccebda0edc49aa74b0248be88c6e1c21f65bad2
daimessdn@gmail.com <daimessdn@gmail.com>
2023-06-26 18:45:08 +0700

    updated variables and descriptions on Vogel calculation

--- vogel.py
+++ vogel.py
@@ -5,41 +5,41 @@ def separate_line():
 
 # Let's test on creating our first performance
 reservoir_pressure = 1734
-production1 = TwoPhaseProduction(reservoir_pressure)
+production_data = TwoPhaseProduction(reservoir_pressure)
 
 # Initial testing in repr production instance
-print(repr(production1))
+print(repr(production_data))
 
 separate_line()
 
 # Add several data on the production instance
-production_data = [
+production_cases = [
     { "q": 252, "p": 1653 },
     { "q": 516, "p": 1507 },
     { "q": 768, "p": 1335 },
 ]
 
-for data in production_data:
-    production1.data.append(data)
+for data in production_cases:
+    production_data.data.append(data)
 
-print("Production data in production 1:")
-print(production1.data)
+print("Here is the production data:")
+print(production_data.data)
 
 separate_line()
 
 # Another testing in repr production instance
-print(repr(production1))
+print(repr(production_data))
 
 separate_line()
 
 # Using Vogel Equation for calculating
 ## max flow rate using single data
-for data in production1.data:
+for data in production_data.data:
     print("q: %d, p: %d" % (data["q"], data["p"]))
 
     print("Max flow rate using Vogel equation: " + str(
         round(
-            production1.calculate_q_max(
+            production_data.calculate_q_max(
                 "vogel",
                 data
             )
